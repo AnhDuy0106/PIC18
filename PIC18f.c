c167c3dce446e98ccdd81b769f0639211b6f2b94
Bùi Nguyễn Anh Duy <126902854+AnhDuy0106@users.noreply.github.com>
2024-05-18 17:49:00 +0700

    Update PIC18f.c

--- PIC18f.c
+++ PIC18f.c
@@ -1,29 +1,24 @@
+#include <pic18f4550.h>  
+#include "lcd.h"
 #include <xc.h>
 #include <stdio.h>
-#include "lcd.h"
-
-#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
-#pragma config WDTEN = OFF      // Watchdog Timer Enable bit (WDT disabled)
 
 #define _XTAL_FREQ 20000000     // Define the system clock
+#define DHT11_PIN RA0 // DHT11 data pin
+#define PIR_PIN   RA1   // PIR sensor pin
+#define BUTTON1   RB0   // Button 1 pin
+#define BUTTON2   RB1// Button 2 pin
 
-#define DHT11_PIN RA0           // DHT11 data pin
-#define PIR_PIN RA1             // PIR sensor pin
-#define BUTTON1 RB0             // Button 1 pin
-#define BUTTON2 RB1             // Button 2 pin
-
-void LCD_Init();
-void DHT11_Init();
-void DHT11_Read(int *temperature, int *humidity);
-unsigned char DHT11_Check();
-unsigned char DHT11_ReadByte();
+void DHT11_Start();
+unsigned char DHT11_CheckResponse();
+unsigned char DHT11_ReadData();
 
 void main(void)
 {
     int temperature = 0, humidity = 0;
     int students = 0;
     char buffer[16];
-    int option = 0; // 0: Temperature & Humidity, 1: Students detected
+    int pir_last_state = 0;
 
     TRISA = 0xFF;  // Set PORTA as input (for DHT11 and PIR)
     TRISB = 0xFF;  // Set PORTB as input (for Buttons)
@@ -37,95 +32,85 @@ void main(void)
         // Check Button 1
         if(BUTTON1 == 0)
         {
-            option = 0; // Temperature & Humidity
-            __delay_ms(100); // Debouncing delay
+            DHT11_Start();
+            if(DHT11_CheckResponse())
+            {
+                unsigned char data[5];
+                int i;
+                
+                for(i = 0; i < 5; i++) {
+                    data[i] = DHT11_ReadData();
+                }
+                
+                if(data[4] == ((data[0] + data[1] + data[2] + data[3]) & 0xFF)) {
+                    humidity = data[0];
+                    temperature = data[2];
+                }
+            }
+            LCD_Clear();
+            LCD_Set_Cursor(1, 1);
+            sprintf(buffer, "Temp: %02dC", temperature);
+            LCD_Write_String(buffer);
+            LCD_Set_Cursor(2, 1);
+            sprintf(buffer, "Hum: %02d%%", humidity);
+            LCD_Write_String(buffer);
+            __delay_ms(200); // Debouncing delay
         }
+
         // Check Button 2
         if(BUTTON2 == 0)
         {
-            option = 1; // Students detected
-            __delay_ms(100); // Debouncing delay
-        }
-
-        if(option == 0) // Temperature & Humidity
-        {
-            DHT11_Read(&temperature, &humidity);  // Read DHT11 values
-            sprintf(buffer, "Temp: %02dC", temperature);
-            LCD_Clear();
-            LCD_Set_Cursor(1, 1);
-            LCD_Write_String(buffer);
-            sprintf(buffer, "Hum: %02d%%", humidity);
-            LCD_Set_Cursor(2, 1);
-            LCD_Write_String(buffer);
-        }
-        else if(option == 1) // Students detected
-        {
-            if(PORTAbits.RA1) // Check PIR sensor
+            if(PIR_PIN && !pir_last_state) // Check PIR sensor
             {
                 students++;
-                __delay_ms(1000); 
+                __delay_ms(1000); // Delay to count only once
             }
+            pir_last_state = PIR_PIN; // Update last PIR state
             LCD_Clear();
             LCD_Set_Cursor(1, 1);
             sprintf(buffer, "Students: %d", students);
             LCD_Write_String(buffer);
+            __delay_ms(300); // Debouncing delay
         }
+
+        __delay_ms(200); // Delay to prevent LCD flickering
     }
 }
 
-void DHT11_Init()
+void DHT11_Start()
 {
     TRISA0 = 0; // Set RA0 as output
-    LATAbits.LATA0 = 0;
-    __delay_ms(18); // Send start signal
-    LATAbits.LATA0 = 1;
-    __delay_us(20);
+    LATAbits.LATA0 = 0; // Pull RA0 low
+    __delay_ms(18); // Wait for 18ms
+    LATAbits.LATA0 = 1; // Pull RA0 high
+    __delay_us(20); // Wait for 20-40us
     TRISA0 = 1; // Set RA0 as input
 }
 
-unsigned char DHT11_Check()
+unsigned char DHT11_CheckResponse()
 {
-    unsigned char response = 0;
     __delay_us(40);
-    if (!PORTAbits.RA0)
+    if (!DHT11_PIN)
     {
         __delay_us(80);
-        if (PORTAbits.RA0) response = 1;
-        __delay_us(40);
-    }
-    return response;
-}
-
-unsigned char DHT11_ReadByte()
-{
-    unsigned char i, byte = 0;
-    for (i = 0; i < 8; i++)
-    {
-        while (!PORTAbits.RA0); // Wait for pin to go high
-        __delay_us(30);
-        if (PORTAbits.RA0) byte |= (1 << (7 - i)); // If high after 30 us, it is a '1'
-        while (PORTAbits.RA0); // Wait for pin to go low
-    }
-    return byte;
-}
-
-void DHT11_Read(int *temperature, int *humidity)
-{
-    unsigned char temp_int, temp_dec, hum_int, hum_dec, checksum;
-
-    DHT11_Init();
-    if (DHT11_Check())
-    {
-        hum_int = DHT11_ReadByte();
-        hum_dec = DHT11_ReadByte();
-        temp_int = DHT11_ReadByte();
-        temp_dec = DHT11_ReadByte();
-        checksum = DHT11_ReadByte();
-
-        if ((hum_int + hum_dec + temp_int + temp_dec) == checksum)
+        if (DHT11_PIN)
         {
-            *temperature = temp_int;
-            *humidity = hum_int;
+            __delay_us(40);
+            return 1;
         }
     }
+    return 0;
+}
+
+unsigned char DHT11_ReadData()
+{
+    unsigned char i, result = 0;
+    for (i = 0; i < 8; i++)
+    {
+        while (!DHT11_PIN); // Wait for pin to go high
+        __delay_us(30);
+        if (DHT11_PIN) result |= (1 << (7 - i));
+        while (DHT11_PIN); // Wait for pin to go low
+    }
+    return result;
 }
